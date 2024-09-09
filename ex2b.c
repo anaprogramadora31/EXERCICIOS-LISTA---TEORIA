1034eabe5557bfebb1dd775e46a0ec93eac948ea
anaprogramadora31 <ana.carolinac@icloud.com>
2024-09-09 20:13:13 -0300

    Add files via upload

---
+++ ex2b.c
@@ -0,0 +1,14 @@
+//Exercicio 2
+//B)
+#include <stdio.h>
+#include <stdlib.h>
+
+ int main () {
+ char nome[100];
+
+ printf ("Olá eu sou o Nº1, como é seu nome?");
+ scanf ("%s",&nome);
+ printf ("Bem-vindo ao clube: %s",nome);
+
+return 0;
+ }
