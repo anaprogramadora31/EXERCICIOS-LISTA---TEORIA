7e22fe5ea790b0a6e84d0d925b438f78730666dd
anaprogramadora31 <ana.carolinac@icloud.com>
2024-09-09 20:17:16 -0300

    Add files via upload

---
+++ ex2d.c
@@ -0,0 +1,19 @@
+//Exercicio 2
+//D)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+ int main () {
+ float raio, area;
+
+ printf ("Escreva o valor do raio:");
+
+ scanf ("%f",&raio);
+ area=raio*raio*3.14;
+
+ printf ("Sua area e %.2f", area);
+
+return 0;
+ }
